4cb9c69a2db2e533785f8cf780a64da6f58b4fec
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-07-03 22:02:03 +0200

    First take

---
+++ src/common/basex.spec.ts
@@ -0,0 +1,12 @@
+describe("BaseX", () => {
+  it("should encode base62", () => {
+    // const buf = Buffer.from("Hello World")
+    // expect(Array.from(buf)).toEqual([
+    //   72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100,
+    // ])
+    // expect(base32Encode(buf)).toBe("91JPRV3F41BPYWKCCG")
+    // expect(baseXEncode(buf)).toBe("91JPRV3F41BPYWKCCG")
+    // // Not tested!
+    // expect(baseXEncode(buf, BASE62_ALPHABET)).toBe("ApJ4xY0lRsd4w")
+  })
+})

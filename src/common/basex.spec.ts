0dcc58c686676ff1c92f75a08658f7b430deb1c4
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-07-04 00:03:40 +0200

    Support nicely

--- src/common/basex.spec.ts
+++ src/common/basex.spec.ts
@@ -1,34 +1,20 @@
 import { useBaseX } from "./basex"
 
-const alphabets = {
-  "2": "01",
-  "8": "01234567",
-  "11": "0123456789a",
-  "16": "0123456789abcdef",
-  "32": "0123456789ABCDEFGHJKMNPQRSTVWXYZ",
-  // "32": "ybndrfg8ejkmcpqxot1uwisza345h769", //  (z-base-32)
-  "36": "0123456789abcdefghijklmnopqrstuvwxyz",
-  "58": "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz",
-  "62": "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  "64": "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
-  "66": "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.!~",
-}
-
 describe("BaseX", () => {
   it("should encode base16", () => {
-    const { encode, decode } = useBaseX(alphabets["16"])
+    const { encode, decode } = useBaseX(16)
     expect(encode([0x01, 0x09, 0x0, 0xff])).toBe("10900ff")
     expect(decode("00010900ff")).toEqual(new Uint8Array([1, 9, 0, 255]))
   })
 
   it("should encode base62", () => {
-    const { encode, decode } = useBaseX(alphabets["62"])
+    const { encode, decode } = useBaseX(62)
     expect(encode([0, 0x01, 0x09, 0x0, 0xff])).toBe("1aS1F")
     expect(decode("01aS1F")).toEqual(new Uint8Array([1, 9, 0, 255]))
   })
 
   it("should encode base62 carry", () => {
-    const { encode, decode } = useBaseX(alphabets["62"])
+    const { encode, decode } = useBaseX(62)
     expect(encode([61])).toBe("Z")
     expect(encode([62])).toBe("10")
     expect(decode("Z")).toEqual(new Uint8Array([61]))
@@ -37,7 +23,7 @@ describe("BaseX", () => {
   })
 
   it("should encode suid length", () => {
-    const { encode, decode } = useBaseX(alphabets["62"])
+    const { encode, decode } = useBaseX(62)
     expect(
       encode([
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
--- src/common/basex.ts
+++ src/common/basex.ts
@@ -4,7 +4,30 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
-export function useBaseX(ALPHABET: string) {
+const alphabets = {
+  "2": "01",
+  "8": "01234567",
+  "11": "0123456789a",
+  "16": "0123456789abcdef",
+  "32": "0123456789ABCDEFGHJKMNPQRSTVWXYZ",
+  // "32": "ybndrfg8ejkmcpqxot1uwisza345h769", //  (z-base-32)
+  "36": "0123456789abcdefghijklmnopqrstuvwxyz",
+  "58": "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz",
+  "62": "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  "64": "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+  "66": "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.!~",
+}
+
+export function useBaseX(alphaOrBase: string | number) {
+  let ALPHABET: string
+  if (typeof alphaOrBase === "string") {
+    ALPHABET = alphaOrBase
+  } else {
+    // @ts-ignore
+    ALPHABET = alphabets[alphaOrBase.toString()]
+    if (ALPHABET == null) throw new Error(`Unknown base ${alphaOrBase}`)
+  }
+
   if (ALPHABET.length >= 255) throw new TypeError("Alphabet too long")
 
   const BASE_MAP = new Uint8Array(256)

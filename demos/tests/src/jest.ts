63b75ce9af387253a5f264823fcf5972ba0f6066
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-08-26 17:47:21 +0200

    Condense not checks

--- demos/tests/src/jest.ts
+++ demos/tests/src/jest.ts
@@ -10,7 +10,7 @@ let perfomedAssertions = 0
 let expectAssertions = -1
 
 export async function describe(title: string, fn: any) {
-  log.info(`${title}...`)
+  log(`${title}...`)
   context = {
     fails: 0,
     its: [],
@@ -32,7 +32,7 @@ export async function describe(title: string, fn: any) {
   }
 
   for (let it of context.its) {
-    log.info(`... ${it.title}`)
+    log(`... ${it.title}`)
 
     try {
       perfomedAssertions = 0
@@ -84,7 +84,7 @@ export function it(title: string, fn: any) {
 function expect(actual: any) {
   function test(ok: boolean, expected: any) {
     if (ok) {
-      log.info("OK - Passed Test")
+      log("OK - Passed Test")
       perfomedAssertions += 1
     } else {
       log.warn(`Fail: got ${actual} expected ${expected}`)
@@ -92,28 +92,47 @@ function expect(actual: any) {
     }
   }
 
-  return {
-    toBe: (expected: any) => test(expected === actual, expected),
-    toEqual: (expected: any) => test(deepEqual(expected, actual), expected),
-    toBeNull: () => test(actual == null, null),
-    toBeTruthy: () => test(actual == true, null),
-    toBeGreaterThan: (expected: number) => test(expected < actual, expected),
-    toContain: (expected: any) => test(actual.includes(expected), expected),
-
-    get not() {
-      return {
-        toBe: (expected: any) => test(expected !== actual, expected),
-        toEqual: (expected: any) =>
-          test(!deepEqual(expected, actual), expected),
-        toBeNull: () => test(actual != null, null),
-        toBeTruthy: () => test(actual != true, null),
-        toBeGreaterThan: (expected: number) =>
-          test(expected >= actual, expected),
-        toContain: (expected: any) =>
-          test(!actual.includes(expected), expected),
-      }
-    },
+  let matchers = {
+    toBe: (expected: any) => expected === actual,
+    toEqual: (expected: any) => deepEqual(expected, actual),
+    toBeNull: () => actual == null,
+    toBeTruthy: () => actual == true,
+    toBeGreaterThan: (expected: number) => expected < actual,
+    toBeLessThan: (expected: number) => expected > actual,
+    toContain: (expected: any) => actual.includes(expected),
+    toHaveLength: (expected: any) => actual.length === expected,
+    // toMatchInlineSnapshot:
   }
+
+  let obj: any = {
+    not: {},
+  }
+
+  for (const [key, value] of Object.entries(matchers)) {
+    obj[key] = (expected: any) => {
+      test(value(expected), expected)
+    }
+    obj.not[key] = (expected: any) => {
+      test(!value(expected), expected)
+    }
+  }
+
+  return obj
+
+  // return {
+  //   toBe: (expected: any) => test(expected !== actual, expected),
+  //   toEqual: (expected: any) =>
+  //     test(!deepEqual(expected, actual), expected),
+  //   toBeNull: () => test(actual != null, null),
+  //   toBeTruthy: () => test(actual != true, null),
+  //   toBeGreaterThan: (expected: number) =>
+  //     test(expected >= actual, expected),
+  //   toBeLessThan: (expected: number) => test(expected <= actual, expected),
+  //   toContain: (expected: any) =>
+  //     test(!actual.includes(expected), expected),
+  //   toHaveLength: (expected: any) =>
+  //     test(actual.length !== expected, expected),
+  // }
 }
 
 expect.assertions = (count: number) => (expectAssertions = count)
@@ -137,7 +156,18 @@ import("../../../src/browser/localhost.spec")
 import("../../../src/common/array.spec")
 import("../../../src/common/basex.spec")
 import("../../../src/common/camelcase.spec")
-
 // import("../../../src/common/channel.spec")
 import("../../../src/common/convert.spec")
-import("../../../src/common/emitter.spec")
+// import("../../../src/common/emitter.spec")
+import("../../../src/common/log-filter.spec")
+// import("../../../src/common/log-util.spec")
+// import("../../../src/common/log.spec")
+import("../../../src/common/mq.spec")
+import("../../../src/common/mutex.spec")
+// import("../../../src/common/orderby.spec")
+// import("../../../src/common/platform.spec")
+import("../../../src/common/promises.spec")
+import("../../../src/common/queue.spec")
+import("../../../src/common/sortable.spec")
+import("../../../src/common/utils.spec")
+import("../../../src/common/uuid.spec")

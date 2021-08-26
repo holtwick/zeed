656435c67486d578a7df9953326676a1345c6a95
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-08-26 17:59:17 +0200

    Migrate some tests

--- src/common/orderby.spec.ts
+++ src/common/orderby.spec.ts
@@ -4,48 +4,36 @@ import { composeOrderby, parseOrderby, sortedOrderby } from "./orderby"
 
 describe("Order by", () => {
   it("should order correctly", () => {
-    expect(parseOrderby("timestamp asc")).toMatchInlineSnapshot(`
-      Object {
-        "asc": true,
-        "desc": false,
-        "field": "timestamp",
-        "orderby": "asc",
-      }
-    `)
-    expect(parseOrderby("timestamp desc")).toMatchInlineSnapshot(`
-      Object {
-        "asc": false,
-        "desc": true,
-        "field": "timestamp",
-        "orderby": "desc",
-      }
-    `)
-    expect(parseOrderby("timestamp xxx")).toMatchInlineSnapshot(`
-      Object {
-        "asc": true,
-        "desc": false,
-        "field": "timestamp",
-        "orderby": "xxx",
-      }
-    `)
-    expect(parseOrderby("timestamp")).toMatchInlineSnapshot(`
-      Object {
-        "asc": true,
-        "desc": false,
-        "field": "timestamp",
-        "orderby": "asc",
-      }
-    `)
+    expect(parseOrderby("timestamp asc")).toEqual({
+      asc: true,
+      desc: false,
+      field: "timestamp",
+      orderby: "asc",
+    })
+    expect(parseOrderby("timestamp desc")).toEqual({
+      asc: false,
+      desc: true,
+      field: "timestamp",
+      orderby: "desc",
+    })
+    expect(parseOrderby("timestamp xxx")).toEqual({
+      asc: true,
+      desc: false,
+      field: "timestamp",
+      orderby: "xxx",
+    })
+    expect(parseOrderby("timestamp")).toEqual({
+      asc: true,
+      desc: false,
+      field: "timestamp",
+      orderby: "asc",
+    })
   })
 
   it("should compose", () => {
-    expect(composeOrderby("timestamp")).toMatchInlineSnapshot(`"timestamp asc"`)
-    expect(composeOrderby("timestamp", true)).toMatchInlineSnapshot(
-      `"timestamp asc"`
-    )
-    expect(composeOrderby("timestamp", false)).toMatchInlineSnapshot(
-      `"timestamp desc"`
-    )
+    expect(composeOrderby("timestamp")).toEqual("timestamp asc")
+    expect(composeOrderby("timestamp", true)).toEqual("timestamp asc")
+    expect(composeOrderby("timestamp", false)).toEqual("timestamp desc")
   })
 
   it("should sort by", () => {
@@ -60,32 +48,28 @@ describe("Order by", () => {
         name: "c",
       },
     ]
-    expect(sortedOrderby(values, "name desc")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "name": "c",
-        },
-        Object {
-          "name": "b",
-        },
-        Object {
-          "name": "a",
-        },
-      ]
-    `)
-    expect(sortedOrderby(values, "name asc")).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "name": "a",
-        },
-        Object {
-          "name": "b",
-        },
-        Object {
-          "name": "c",
-        },
-      ]
-    `)
+    expect(sortedOrderby(values, "name desc")).toEqual([
+      {
+        name: "c",
+      },
+      {
+        name: "b",
+      },
+      {
+        name: "a",
+      },
+    ])
+    expect(sortedOrderby(values, "name asc")).toEqual([
+      {
+        name: "a",
+      },
+      {
+        name: "b",
+      },
+      {
+        name: "c",
+      },
+    ])
   })
 
   it("should sort by", () => {
@@ -107,47 +91,42 @@ describe("Order by", () => {
         name: "d",
       },
     ]
-    expect(sortedOrderby(values, "group desc", "name asc"))
-      .toMatchInlineSnapshot(`
-Array [
-  Object {
-    "group": 2,
-    "name": "c",
-  },
-  Object {
-    "group": 1,
-    "name": "a",
-  },
-  Object {
-    "group": 1,
-    "name": "b",
-  },
-  Object {
-    "group": 1,
-    "name": "d",
-  },
-]
-`)
+    expect(sortedOrderby(values, "group desc", "name asc")).toEqual([
+      {
+        group: 2,
+        name: "c",
+      },
+      {
+        group: 1,
+        name: "a",
+      },
+      {
+        group: 1,
+        name: "b",
+      },
+      {
+        group: 1,
+        name: "d",
+      },
+    ])
 
-    expect(sortedOrderby(values, "group", "name desc")).toMatchInlineSnapshot(`
-Array [
-  Object {
-    "group": 1,
-    "name": "d",
-  },
-  Object {
-    "group": 1,
-    "name": "b",
-  },
-  Object {
-    "group": 1,
-    "name": "a",
-  },
-  Object {
-    "group": 2,
-    "name": "c",
-  },
-]
-`)
+    expect(sortedOrderby(values, "group", "name desc")).toEqual([
+      {
+        group: 1,
+        name: "d",
+      },
+      {
+        group: 1,
+        name: "b",
+      },
+      {
+        group: 1,
+        name: "a",
+      },
+      {
+        group: 2,
+        name: "c",
+      },
+    ])
   })
 })

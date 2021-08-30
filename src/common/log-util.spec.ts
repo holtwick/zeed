039a71eeddacd65ee27c917384450fedfda9347a
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-08-30 14:38:36 +0200

    Fix test

--- src/common/log-util.spec.ts
+++ src/common/log-util.spec.ts
@@ -5,7 +5,7 @@ import { getSourceLocation, getStackLlocationList } from "./log-util"
 describe("log-util", function () {
   test("should find correct source file line", function () {
     const source = getSourceLocation(1, true)
-    expect(source).toBe("src/common/log-util.spec.ts:7:20")
+    expect(source).toBe("src/common/log-util.spec.ts:7:37")
   })
 
   test("should parse stack", function () {

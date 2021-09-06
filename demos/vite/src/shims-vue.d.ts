6ddfe19b268e9ce3d84dc39a20328a13ba9eb192
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-09-06 10:19:02 +0200

    Prettier

--- demos/vite/src/shims-vue.d.ts
+++ demos/vite/src/shims-vue.d.ts
@@ -1,5 +1,5 @@
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
+declare module "*.vue" {
+  import { DefineComponent } from "vue"
   const component: DefineComponent<{}, {}, any>
   export default component
 }
---
+++ src/common/msg/bridge.spec.ts
@@ -0,0 +1,12 @@
+// These can be emitted from both the Client or the Server
+interface BridgeBoth {}
+
+// These are emitted by the client to the service
+interface BridgeClientToServer extends BridgeBoth {}
+
+// These are emitted by the service to the client
+interface BridgeServiceToClient extends BridgeBoth {}
+
+describe("bridge", () => {
+  it("should ", () => {})
+})

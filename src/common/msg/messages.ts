b32b18b205c28359a3e5146674a4ee79bcfb841a
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-09-27 20:22:48 +0200

    Async encoder

--- src/common/msg/messages.ts
+++ src/common/msg/messages.ts
@@ -39,177 +39,179 @@ export type MessageHub = {
   send<L extends object>(): MessagesMethods<L>
 }
 
-/** @deprecated */
-export function useMessages<L extends object>(
-  opt: {
-    channel?: Channel
-    encoder?: Encoder
-    handlers?: L
-    retryAfter?: number
-    ignoreUnhandled?: boolean
-  } = {}
-): MessagesMethods<L> {
-  let {
-    handlers,
-    encoder = new JsonEncoder(),
-    retryAfter = 1000,
-    ignoreUnhandled = true,
-  } = opt
+// /** @deprecated */
+// export function useMessages<L extends object>(
+//   opt: {
+//     channel?: Channel
+//     encoder?: Encoder
+//     handlers?: L
+//     retryAfter?: number
+//     ignoreUnhandled?: boolean
+//   } = {}
+// ): MessagesMethods<L> {
+//   let {
+//     handlers,
+//     encoder = new JsonEncoder(),
+//     retryAfter = 1000,
+//     ignoreUnhandled = true,
+//   } = opt
 
-  const log = Logger(`messages:${uname(!!handlers ? "server" : "client")}`)
+//   const log = Logger(`messages:${uname(!!handlers ? "server" : "client")}`)
 
-  let channel: Channel | undefined
-  let queue: Message[] = []
-  let queueRetryTimer: any
-  let waitingForResponse: any = {}
+//   let channel: Channel | undefined
+//   let queue: Message[] = []
+//   let queueRetryTimer: any
+//   let waitingForResponse: any = {}
 
-  const dispose = () => {
-    clearTimeout(queueRetryTimer)
-  }
+//   const dispose = () => {
+//     clearTimeout(queueRetryTimer)
+//   }
 
-  const postNext = () => {
-    clearTimeout(queueRetryTimer)
-    if (channel) {
-      if (channel.isConnected) {
-        while (queue.length) {
-          let message = queue[0]
-          try {
-            channel.postMessage(encoder.encode(message))
-            queue.shift() // remove from queue when done
-          } catch (err) {
-            log.warn("postMessage", err)
-            break
-          }
-        }
-      }
-      if (queue.length > 0 && retryAfter > 0) {
-        queueRetryTimer = setTimeout(postNext, retryAfter)
-      }
-    }
-  }
+//   const postNext = async () => {
+//     clearTimeout(queueRetryTimer)
+//     if (channel) {
+//       if (channel.isConnected) {
+//         while (queue.length) {
+//           let message = queue[0]
+//           try {
+//             channel.postMessage(await promisify(encoder.encode(message)))
+//             queue.shift() // remove from queue when done
+//           } catch (err) {
+//             log.warn("postMessage", err)
+//             break
+//           }
+//         }
+//       }
+//       if (queue.length > 0 && retryAfter > 0) {
+//         queueRetryTimer = setTimeout(postNext, retryAfter)
+//       }
+//     }
+//   }
 
-  const postMessage = (message: Message) => {
-    log("enqueue postMessage", message)
-    queue.push(message)
-    postNext()
-  }
+//   const postMessage = (message: Message) => {
+//     log("enqueue postMessage", message)
+//     queue.push(message)
+//     postNext()
+//   }
 
-  const connect = (newChannel: Channel) => {
-    channel = newChannel
-    channel.on("connect", postNext)
+//   const connect = async (newChannel: Channel) => {
+//     channel = newChannel
+//     channel.on("connect", postNext)
 
-    // Server side
+//     // Server side
 
-    if (handlers) {
-      channel.on("message", async (msg: any) => {
-        const { name, args, id } = encoder.decode(msg.data) as any
-        if (name) {
-          log(`name ${name} id ${id}`)
-          try {
-            // @ts-ignore
-            let result = await promisify(handlers[name](...args))
-            log(`result ${result}`)
-            if (id) {
-              postMessage({ id, result })
-            }
-          } catch (error) {
-            log("execution error", error)
-            let err =
-              error instanceof Error ? error : new Error(valueToString(error))
-            postMessage({
-              id,
-              error: {
-                message: err.message,
-                stack: err.stack,
-                name: err.name,
-              },
-            })
-          }
-        } else if (!ignoreUnhandled) {
-          log.warn("Unhandled message", msg)
-        }
-      })
-    }
+//     if (handlers) {
+//       channel.on("message", async (msg: any) => {
+//         const { name, args, id } = await promisify(encoder.decode(msg.data))
+//         if (name) {
+//           log(`name ${name} id ${id}`)
+//           try {
+//             // @ts-ignore
+//             let result = await promisify(handlers[name](...args))
+//             log(`result ${result}`)
+//             if (id) {
+//               postMessage({ id, result })
+//             }
+//           } catch (error) {
+//             log("execution error", error)
+//             let err =
+//               error instanceof Error ? error : new Error(valueToString(error))
+//             postMessage({
+//               id,
+//               error: {
+//                 message: err.message,
+//                 stack: err.stack,
+//                 name: err.name,
+//               },
+//             })
+//           }
+//         } else if (!ignoreUnhandled) {
+//           log.warn("Unhandled message", msg)
+//         }
+//       })
+//     }
 
-    // Client side
+//     // Client side
 
-    channel.on("message", async (msg: any) => {
-      const { name, args, id, result, error } = encoder.decode(msg.data) as any
-      if (!name && id) {
-        log(`response for id=${id}: error=${error}, result=$`, result)
-        if (id in waitingForResponse) {
-          const [resolve, reject] = waitingForResponse[id]
-          if (resolve && reject) {
-            delete waitingForResponse[id]
-            if (error) {
-              let err = new Error(error.message)
-              err.stack = error.stack
-              err.name = error.name
-              log("reject", err)
-              reject(err)
-            } else {
-              log("resolve", result)
-              resolve(result)
-            }
-          }
-        }
-      }
-    })
+//     channel.on("message", async (msg: any) => {
+//       const { name, args, id, result, error } = await promisify(
+//         encoder.decode(msg.data)
+//       )
+//       if (!name && id) {
+//         log(`response for id=${id}: error=${error}, result=$`, result)
+//         if (id in waitingForResponse) {
+//           const [resolve, reject] = waitingForResponse[id]
+//           if (resolve && reject) {
+//             delete waitingForResponse[id]
+//             if (error) {
+//               let err = new Error(error.message)
+//               err.stack = error.stack
+//               err.name = error.name
+//               log("reject", err)
+//               reject(err)
+//             } else {
+//               log("resolve", result)
+//               resolve(result)
+//             }
+//           }
+//         }
+//       }
+//     })
 
-    postNext()
-  }
+//     postNext()
+//   }
 
-  const fetchMessage = async (
-    name: string,
-    args: any[],
-    opt: MessagesOptions = {}
-  ) => {
-    const { timeout = 5000 } = opt
-    const id = uuid()
-    postMessage({
-      name,
-      args,
-      id,
-    })
-    return tryTimeout(
-      new Promise(
-        (resolve, reject) => (waitingForResponse[id] = [resolve, reject])
-      ),
-      timeout
-    )
-  }
+//   const fetchMessage = async (
+//     name: string,
+//     args: any[],
+//     opt: MessagesOptions = {}
+//   ) => {
+//     const { timeout = 5000 } = opt
+//     const id = uuid()
+//     postMessage({
+//       name,
+//       args,
+//       id,
+//     })
+//     return tryTimeout(
+//       new Promise(
+//         (resolve, reject) => (waitingForResponse[id] = [resolve, reject])
+//       ),
+//       timeout
+//     )
+//   }
 
-  if (opt.channel) {
-    connect(opt.channel)
-  }
+//   if (opt.channel) {
+//     connect(opt.channel)
+//   }
 
-  // The async proxy, waiting for a response
-  const createPromiseProxy = (
-    opt: MessagesOptions,
-    predefinedMethods: any = {}
-  ): L => {
-    const { timeout = 5000 } = opt
-    return new Proxy<L>(predefinedMethods, {
-      get: (target: any, name: any) => {
-        if (name in target) return target[name]
-        return (...args: any): any => {
-          if (!handlers) {
-            return fetchMessage(name, args, opt)
-          }
-        }
-      },
-    })
-  }
+//   // The async proxy, waiting for a response
+//   const createPromiseProxy = (
+//     opt: MessagesOptions,
+//     predefinedMethods: any = {}
+//   ): L => {
+//     const { timeout = 5000 } = opt
+//     return new Proxy<L>(predefinedMethods, {
+//       get: (target: any, name: any) => {
+//         if (name in target) return target[name]
+//         return (...args: any): any => {
+//           if (!handlers) {
+//             return fetchMessage(name, args, opt)
+//           }
+//         }
+//       },
+//     })
+//   }
 
-  // The regular proxy without responding, just send
-  return createPromiseProxy({}, {
-    dispose,
-    connect,
-    options(perCallopt: MessagesOptions) {
-      return createPromiseProxy({ ...perCallopt })
-    },
-  } as MessagesDefaultMethods<L>) as MessagesMethods<L>
-}
+//   // The regular proxy without responding, just send
+//   return createPromiseProxy({}, {
+//     dispose,
+//     connect,
+//     options(perCallopt: MessagesOptions) {
+//       return createPromiseProxy({ ...perCallopt })
+//     },
+//   } as MessagesDefaultMethods<L>) as MessagesMethods<L>
+// }
 
 // The async proxy, waiting for a response
 export const createPromiseProxy = <P extends object>(
@@ -252,14 +254,14 @@ export function useMessageHub(
     clearTimeout(queueRetryTimer)
   }
 
-  const postNext = () => {
+  const postNext = async () => {
     clearTimeout(queueRetryTimer)
     if (channel) {
       if (channel.isConnected) {
         while (queue.length) {
           let message = queue[0]
           try {
-            channel.postMessage(encoder.encode(message))
+            channel.postMessage(await encoder.encode(message))
             queue.shift() // remove from queue when done
           } catch (err) {
             log.warn("postMessage", err)
@@ -279,11 +281,11 @@ export function useMessageHub(
     postNext()
   }
 
-  const connect = (newChannel: Channel) => {
+  const connect = async (newChannel: Channel) => {
     channel = newChannel
     channel.on("connect", postNext)
     channel.on("message", async (msg: any) => {
-      const { name, args, id, result, error } = encoder.decode(msg.data) as any
+      const { name, args, id, result, error } = await encoder.decode(msg.data)
 
       // Incoming new message
       if (name) {

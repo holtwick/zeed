5f405c2da58ea68f94d9aa3eb8036cf4832bb21f
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-09-28 20:57:10 +0200

    Time calculations

--- src/common/time.ts
+++ src/common/time.ts
@@ -1,5 +1,7 @@
 // (C)opyright 2021-07-15 Dirk Holtwick, holtwick.it. All rights reserved.
 
+import { time } from "console"
+
 /**
  * @returns Timestamp in miliseconds
  */
@@ -9,4 +11,47 @@ export const getTimestamp = (): number =>
 
 export function formatMilliseconds(ms: number): string {
   return ms > 999 ? (ms / 1000).toFixed(1) + "s" : ms.toFixed(2) + "ms"
+}
+
+export const DAY_MS = 1000 * 60 * 60 * 24
+
+export function getDayOffset<T = number>(
+  offset: number = 0,
+  relativeToDate: Date = new Date(),
+  mode?: (timestamp: number) => T
+): T {
+  let tdate = new Date(relativeToDate.getTime())
+  tdate.setHours(0)
+  tdate.setMinutes(0)
+  tdate.setSeconds(0)
+  tdate.setMilliseconds(0)
+  const ms = tdate.getTime()
+  const ts = ms - offset * DAY_MS
+  if (mode) return mode(ts)
+  return ts as any
+}
+
+export function getDayOffsetISO(
+  offset: number = 0,
+  relativeToDate: Date = new Date()
+): string {
+  return getDayOffset(offset, relativeToDate, (timestamp) =>
+    new Date(timestamp).toISOString().substr(0, 10)
+  )
+}
+
+export function getDayOffsetYYYYDDMM(
+  offset: number = 0,
+  relativeToDate: Date = new Date()
+): string {
+  return getDayOffset(
+    offset,
+    relativeToDate,
+    (timestamp) =>
+      new Date(timestamp)
+        .toISOString()
+        .substr(0, 10)
+        .replace("-", "")
+        .replace("-", "") // replace 2 times OMG
+  )
 }

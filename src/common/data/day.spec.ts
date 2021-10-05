81fd3443fb6000856a4745ad2add9a60cb133dfc
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-10-05 17:39:58 +0200

    Avoid replaceAll

--- src/common/data/day.spec.ts
+++ src/common/data/day.spec.ts
@@ -73,38 +73,4 @@ Array [
 
     // expect(new Date().toISOString().startsWith(today().toString())).toBe(true)
   })
-
-  // it("should calc correctly", () => {
-  //   let yesterday = getDayOffset(-1)
-  //   let today = getDayOffset()
-  //   expect(yesterday).toBeLessThan(today)
-  // })
-
-  // it("should convert to Days", () => {
-  //   let date = new Date("1987-12-31T00:02:03")
-  //   let days = daysFromLocalDate(date)
-  //   expect(days).toBe(19871231)
-  //   expect(toDate(days).toDateString()).toEqual(date.toDateString())
-  // })
-
-  // it("should respect timezone", () => {
-  //   let ts = new Date("2020-01-01T00:00:00Z").getTime()
-  //   expect(getDayOffset(0, new Date("2020-01-01T00:00:00Z"))).toEqual(ts)
-  //   expect(getDayOffset(0, new Date("2020-01-01T02:00:00Z"))).toEqual(ts)
-  //   expect(getDayOffset(0, new Date("2019-12-31T23:59:59Z"))).toEqual(
-  //     ts - DAY_MS
-  //   )
-  //   expect(getDayOffset(-1, new Date("2020-01-01T00:00:00Z"))).toEqual(
-  //     ts - DAY_MS
-  //   )
-  //   expect(getDayOffsetYYYYDDMM(0, new Date("2020-01-01T00:00:00Z"))).toEqual(
-  //     "20200101"
-  //   )
-  //   expect(getDayOffsetYYYYDDMM(-1, new Date("2020-01-01T00:00:00Z"))).toEqual(
-  //     "20191231"
-  //   )
-  //   expect(getDayOffsetISO(-1, new Date("2020-01-01T00:00:00Z"))).toEqual(
-  //     "2019-12-31"
-  //   )
-  // })
 })

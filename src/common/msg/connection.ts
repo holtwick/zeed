bdfa8df943ef8063b1b614674e2d2e3a475756a6
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-11-02 16:22:56 +0100

    Closer to messages API

--- src/common/msg/connection.ts
+++ src/common/msg/connection.ts
@@ -1,26 +1,38 @@
 import { Logger } from "../log"
-import { uuid } from "../uuid"
+import { uname } from "../uuid"
 import { Channel } from "./channel"
 import { DefaultListener, Emitter, ListenerSignature } from "./emitter"
+import { Encoder, JsonEncoder } from "./encoder"
+
+interface ConnectionConfig {
+  channel: Channel
+  encoder?: Encoder
+  name?: string
+}
 
 export class Connection<
   L extends ListenerSignature<L> = DefaultListener
 > extends Emitter<L> {
-  id: string
+  name: string
   channel: Channel
+  encoder: Encoder
   log: any
 
   get shortId() {
-    return this.id.substr(0, 6)
+    return this.name.substr(0, 6)
   }
 
-  constructor(channel: Channel) {
+  constructor(opt: ConnectionConfig) {
     super()
 
-    this.id = channel.id ?? uuid()
-    this.log = Logger(`${this.shortId}`)
+    let { name, encoder = new JsonEncoder(), channel } = opt
 
     this.channel = channel
+    this.encoder = encoder
+
+    this.name = name ?? this.channel.id ?? uname("conn")
+    this.log = Logger(`${this.shortId}`)
+
     this.channel.on("connect", () => {
       this.log("channel connected")
     })
@@ -28,8 +40,8 @@ export class Connection<
       this.log("channel disconnected")
     })
 
-    this.channel.on("message", ({ data }) => {
-      let info = JSON.parse(data)
+    this.channel.on("message", async ({ data }) => {
+      let info = await this.encoder.decode(data)
       this.log(`channel message, event=${info?.event}`)
       if (info) {
         const { event, args } = info
@@ -48,11 +60,18 @@ export class Connection<
         this.log.warn("channel not connected")
         return false
       }
-      this.channel.postMessage(JSON.stringify({ event, args }))
+      const data = await this.encoder.encode({ event, args })
+      this.channel.postMessage(data)
       return true
     } catch (err) {
       this.log.warn(`emit(${event})`, err)
     }
     return false
   }
+}
+
+export function useConnection<L extends ListenerSignature<L> = DefaultListener>(
+  opt: ConnectionConfig
+) {
+  return new Connection<L>(opt)
 }

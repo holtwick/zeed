c474e5e43ede6d7600d4b89d61e6e53e67d5fd05
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-11-12 11:14:29 +0100

    Test camelcase variants

--- src/common/data/camelcase.spec.ts
+++ src/common/data/camelcase.spec.ts
@@ -1,8 +1,13 @@
 // (C)opyright 2021-07-15 Dirk Holtwick, holtwick.it. All rights reserved.
 
-import { toCamelCase } from "./camelcase"
+import {
+  fromCamelCase,
+  toCamelCase,
+  toCapitalize,
+  toCapitalizeWords,
+} from "./camelcase"
 
-describe("camelcasex", () => {
+describe("camelcase", () => {
   it("should convert", () => {
     expect(
       [
@@ -12,12 +17,70 @@ describe("camelcasex", () => {
         "-camel-case-classic",
         "ONLY_CAPTITAL_LETTERS_123",
       ].map(toCamelCase)
-    ).toEqual([
-      "spacesSomewhere",
-      "bigOnesAtStart",
-      "camelCaseClassic123",
-      "camelCaseClassic",
-      "onlyCaptitalLetters123",
-    ])
+    ).toMatchInlineSnapshot(`
+Array [
+  "spacesSomewhere",
+  "bigOnesAtStart",
+  "camelCaseClassic123",
+  "camelCaseClassic",
+  "onlyCaptitalLetters123",
+]
+`)
+  })
+
+  it("should capitalize", () => {
+    expect(
+      [
+        "this is a test",
+        "Capital first",
+        "ALL BIG",
+        "5tart with number and endin6",
+      ].map(toCapitalize)
+    ).toMatchInlineSnapshot(`
+Array [
+  "This is a test",
+  "Capital first",
+  "All big",
+  "5tart with number and endin6",
+]
+`)
+  })
+
+  it("should capitalize words", () => {
+    expect(
+      [
+        "this is a test",
+        "Capital first",
+        "ALL BIG",
+        "5tart with number and endin6",
+      ].map(toCapitalizeWords)
+    ).toMatchInlineSnapshot(`
+Array [
+  "This Is A Test",
+  "Capital First",
+  "All Big",
+  "5tart With Number And Endin6",
+]
+`)
+  })
+
+  it("should convert back", () => {
+    expect(
+      [
+        "spacesSomewhere",
+        "bigOnesAtStart",
+        "camelCaseClassic123",
+        "camelCaseClassic",
+        "onlyCaptitalLetters123",
+      ].map((s) => fromCamelCase(s))
+    ).toMatchInlineSnapshot(`
+Array [
+  "spaces-somewhere",
+  "big-ones-at-start",
+  "camel-case-classic123",
+  "camel-case-classic",
+  "only-captital-letters123",
+]
+`)
   })
 })

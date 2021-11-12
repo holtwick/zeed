71fc2ab05367ea9e3b2e1ceb4a3738ae0cabbd3b
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-11-12 11:14:19 +0100

    Fix crypto test

--- src/common/crypto.spec.ts
+++ src/common/crypto.spec.ts
@@ -1,13 +1,19 @@
 // (C)opyright 2021-07-15 Dirk Holtwick, holtwick.it. All rights reserved.
 
-import { randomBytes } from "crypto"
+import { webcrypto } from "crypto"
+import { randomUint8Array } from "./crypto"
 import { equalBinary } from "./data/bin"
+
+if (globalThis.crypto == null) {
+  // @ts-ignore
+  globalThis.crypto = webcrypto
+}
 
 describe("crypto", () => {
   it("should not have collisions", () => {
     expect(equalBinary(new Uint8Array(2), new Uint8Array([0, 0]))).toBe(true)
     let list: Uint8Array[] = Array.apply(null, Array(100)).map(() =>
-      randomBytes(8)
+      randomUint8Array(8)
     )
     let id: Uint8Array | undefined
     while ((id = list.pop())) {

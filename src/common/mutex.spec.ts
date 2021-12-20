d03186044eb81d617f09643e79c1f7665aed7e17
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-12-20 21:47:03 +0100

    Async mutex

--- src/common/mutex.spec.ts
+++ src/common/mutex.spec.ts
@@ -1,20 +1,47 @@
 // (C)opyright 2021-07-15 Dirk Holtwick, holtwick.it. All rights reserved.
 
-import { createMutex } from "./mutex"
+import { useMutex, useAsyncMutex } from "./mutex"
 
-describe("Mutex", () => {
-  it("should lock", () => {
+describe("mutex", () => {
+  it("should mutex", () => {
     let ctr = 0
-    let m = createMutex()
+    let m = useMutex()
     m(() => {
       ++ctr
-      m(() => {
-        ++ctr
-        m(() => {
+      m(
+        () => {
           ++ctr
-        })
-      })
+          m(
+            () => {
+              ++ctr
+            },
+            () => (ctr += 10) // never reached
+          )
+        },
+        () => (ctr += 10)
+      )
     })
-    expect(ctr).toBe(1)
+    expect(ctr).toBe(11)
+  })
+
+  it("should async mutex", async () => {
+    let ctr = 0
+    let m = useAsyncMutex()
+    await m(async () => {
+      ++ctr
+      await m(
+        async () => {
+          ++ctr
+          await m(
+            async () => {
+              ++ctr
+            },
+            async () => (ctr += 10) // never reached
+          )
+        },
+        async () => (ctr += 10)
+      )
+    })
+    expect(ctr).toBe(11)
   })
 })

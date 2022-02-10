f877b822b701d94ff18e3540996fb67ab57f092c
Dirk Holtwick <dirk.holtwick@gmail.com>
2022-02-10 16:41:33 +0100

    Integration and optimization of birpc

---
+++ src/common/msg/rpc.spec.ts
@@ -0,0 +1,61 @@
+import { MessageChannel } from "worker_threads"
+import { useRPC } from "./rpc"
+
+it("basic", async () => {
+  const channel = new MessageChannel()
+
+  const serialize = (data: any) => JSON.stringify(data)
+  const deserialize = (data: any) => JSON.parse(data)
+
+  const bob = useRPC<AliceFunctions>(Bob, {
+    post: (data) => channel.port1.postMessage(data),
+    on: (data) => channel.port1.on("message", data),
+    serialize,
+    deserialize,
+  })
+
+  const alice = useRPC<BobFunctions>(Alice, {
+    // mark bob's `bump` as an event without response
+    eventNames: ["bump"],
+    post: (data) => channel.port2.postMessage(data),
+    on: (data) => channel.port2.on("message", data),
+    serialize,
+    deserialize,
+  })
+
+  // RPCs
+  expect(await bob.hello("Bob")).toEqual("Hello Bob, my name is Alice")
+  expect(await alice.hi("Alice")).toEqual("Hi Alice, I am Bob")
+
+  // one-way event
+  expect(alice.bump()).toBeUndefined()
+
+  expect(Bob.getCount()).toBe(0)
+  await new Promise((resolve) => setTimeout(resolve, 1))
+  expect(Bob.getCount()).toBe(1)
+})
+
+//
+
+let bobCount = 0
+
+const Bob = {
+  hi(name: string) {
+    return `Hi ${name}, I am Bob`
+  },
+  bump() {
+    bobCount += 1
+  },
+  getCount() {
+    return bobCount
+  },
+}
+
+const Alice = {
+  hello(name: string) {
+    return `Hello ${name}, my name is Alice`
+  },
+}
+
+type BobFunctions = typeof Bob
+type AliceFunctions = typeof Alice
---
+++ src/common/msg/rpc.ts
@@ -0,0 +1,114 @@
+// From https://github.com/antfu/birpc/blob/main/src/index.ts MIT
+
+export type ArgumentsType<T> = T extends (...args: infer A) => any ? A : never
+export type ReturnType<T> = T extends (...args: any) => infer R ? R : never
+
+export interface RPCOptions<Remote> {
+  /** No return values expected */
+  onlyEvents?: boolean
+  /** Names of remote functions that do not need response. */
+  eventNames?: (keyof Remote)[]
+  /** Function to post raw message */
+  post: (data: any) => void
+  /** Listener to receive raw message */
+  on: (fn: (data: any) => void) => void
+  /** Custom function to serialize data */
+  serialize?: (data: any) => any
+  /** Custom function to deserialize data */
+  deserialize?: (data: any) => any
+}
+
+export type RPCFn<T> = {
+  /** Call the remote function and wait for the result. */
+  (...args: ArgumentsType<T>): Promise<Awaited<ReturnType<T>>>
+  /** Send event without asking for response */
+  asEvent(...args: ArgumentsType<T>): void
+}
+
+export type RPCReturn<RemoteFunctions> = {
+  [K in keyof RemoteFunctions]: RPCFn<RemoteFunctions[K]>
+}
+
+let rpcCounter = 1
+
+enum RPCMode {
+  request = 1,
+  event = 2,
+  resolve = 3,
+  reject = 4,
+}
+
+type RPCMessage = [
+  RPCMode,
+  any, // args
+  number | undefined | null, // id
+  string | undefined | null // method
+]
+
+export function useRPC<RemoteFunctions = {}, LocalFunctions = {}>(
+  functions: LocalFunctions,
+  options: RPCOptions<RemoteFunctions>
+): RPCReturn<RemoteFunctions> {
+  const {
+    post,
+    on,
+    eventNames = [],
+    serialize = (i) => i,
+    deserialize = (i) => i,
+  } = options
+
+  const rpcPromiseMap = new Map<
+    number,
+    { resolve: (...args: any) => any; reject: (...args: any) => any }
+  >()
+
+  on(async (data) => {
+    const msg = deserialize(data) as RPCMessage
+    const [mode, args, id, method] = msg
+    if (mode === RPCMode.request || mode === RPCMode.event) {
+      let result, error: any
+      try {
+        // @ts-expect-error
+        result = await functions[method](...args)
+      } catch (e) {
+        error = String(e)
+      }
+      if (mode === RPCMode.request && id) {
+        if (error) {
+          post(serialize([RPCMode.reject, error, id]))
+        } else {
+          post(serialize([RPCMode.resolve, result, id]))
+        }
+      }
+    } else if (id) {
+      const promise = rpcPromiseMap.get(id)
+      if (mode === RPCMode.reject) promise?.reject(args)
+      else promise?.resolve(args)
+      rpcPromiseMap.delete(id)
+    }
+  })
+
+  return new Proxy(
+    {},
+    {
+      get(_, method) {
+        const sendEvent = (...args: any[]) => {
+          post(serialize([RPCMode.event, args, null, method]))
+        }
+        if (options.onlyEvents || eventNames.includes(method as any)) {
+          sendEvent.asEvent = sendEvent
+          return sendEvent
+        }
+        const sendCall = (...args: any[]) => {
+          return new Promise((resolve, reject) => {
+            const id = rpcCounter++
+            rpcPromiseMap.set(id, { resolve, reject })
+            post(serialize([RPCMode.request, args, id, method]))
+          })
+        }
+        sendCall.asEvent = sendEvent
+        return sendCall
+      },
+    }
+  ) as any
+}

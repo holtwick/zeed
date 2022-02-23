d4c803be04288c0759b483881d5bae68e0eed787
Dirk Holtwick <dirk.holtwick@gmail.com>
2022-02-23 17:14:13 +0100

    Update and cleanup

--- src/common/platform.spec.ts
+++ src/common/platform.spec.ts
@@ -5,7 +5,7 @@ import { platform } from "./platform"
 describe("Platform", () => {
   it("should detect", () => {
     expect(platform.node).toBe(true)
-    expect(platform.jest).toBe(true)
+    expect(platform.test).toBe(true)
     expect(platform.browser).toBe(false)
 
     let isBrowser = new Function(

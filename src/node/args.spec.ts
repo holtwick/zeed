46d4a5a188ef18fedca0005726c80556eddae21e
Dirk Holtwick <dirk.holtwick@gmail.com>
2022-07-18 23:31:35 +0200

    Args camel case

--- src/node/args.spec.ts
+++ src/node/args.spec.ts
@@ -2,25 +2,36 @@ import { parseArgs } from "./args"
 
 describe("args.spec", () => {
   it("should parse correctly", async () => {
-    let result = parseArgs([
-      "file",
-      "--on",
-      "-i",
-      "-o",
-      "string",
-      "--some=test",
-      "--some-number=123",
-      '--some-one="empty string"',
-    ])
+    let result = parseArgs(
+      [
+        "file",
+        "--on",
+        "-i",
+        "-o",
+        "string",
+        "--some=test",
+        "--some=test2",
+        "--some-more=test3",
+        "--some-number=123",
+        '--some-one="empty string"',
+      ],
+      {
+        some: ["some-more"],
+      }
+    )
     expect(result).toMatchInlineSnapshot(`
       {
-        "--on": true,
-        "--some": "test",
-        "--some-number": "123",
-        "--some-one": "\\"empty string\\"",
-        "-i": true,
-        "-o": "string",
         "file": true,
+        "i": true,
+        "o": "string",
+        "on": true,
+        "some": [
+          "test",
+          "test2",
+          "test3",
+        ],
+        "someNumber": "123",
+        "someOne": "\\"empty string\\"",
       }
     `)
   })
--- src/node/args.ts
+++ src/node/args.ts
@@ -1,37 +1,65 @@
 // Inspired by https://github.com/kof/node-argsparser/blob/master/lib/argsparser.js
 
-export function parseArgs(args?: string[]) {
+import { toCamelCase } from "../common"
+
+interface ParseConfig {
+  alias?: Record<string, string[]>
+}
+
+export function parseArgs(
+  args?: string[],
+  alias: Record<string, string[]> = {}
+) {
   let opts: Record<string, any> = {}
   let curSwitch: string | undefined
 
-  // args is optional, default is process.argv
+  const normalize = toCamelCase
+
+  let nameToAlias = Object.entries(alias).reduce((map, curr) => {
+    let [name, values] = curr
+    if (typeof values === "string") values = [values]
+    for (let value of values) {
+      map[normalize(value)] = normalize(name)
+    }
+    return map
+  }, {} as any)
+
   args = args ?? process.argv
 
   for (let arg of args) {
-    if (/^(-|--)/.test(arg) || !curSwitch) {
-      if (arg.includes("=")) {
-        let [name, value] = arg.split("=", 2)
-        opts[name] = value
-      } else {
-        opts[arg] = true
-        curSwitch = arg
-      }
-      continue
-    }
-
     let value: any = arg
-    if (arg === "false") {
-      value = false
-    } else if (arg === "true") {
-      value = true
+
+    if (/^--?/.test(arg) || curSwitch == null) {
+      curSwitch = arg.replace(/^--?/, "")
+
+      if (arg.includes("=")) {
+        let [name, valuePart] = curSwitch.split("=", 2)
+        curSwitch = name.trim()
+        value = valuePart.trim()
+      } else {
+        value = true
+      }
+
+      curSwitch = normalize(curSwitch)
+      curSwitch = nameToAlias[curSwitch] ?? curSwitch
     }
 
-    if (typeof opts[curSwitch] === "boolean") {
-      opts[curSwitch] = value
-    } else if (Array.isArray(opts[curSwitch])) {
-      opts[curSwitch].push(value)
-    } else {
-      opts[curSwitch] = [opts[curSwitch], value]
+    if (curSwitch != null) {
+      if (arg === "false") {
+        value = false
+      } else if (arg === "true") {
+        value = true
+      }
+
+      if (opts[curSwitch] == null) {
+        opts[curSwitch] = value
+      } else if (typeof opts[curSwitch] === "boolean") {
+        opts[curSwitch] = value
+      } else if (Array.isArray(opts[curSwitch])) {
+        opts[curSwitch].push(value)
+      } else {
+        opts[curSwitch] = [opts[curSwitch], value]
+      }
     }
   }
 

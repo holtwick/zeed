866df2af723e2545a8e887b141ab106a04630c67
Dirk Holtwick <dirk.holtwick@gmail.com>
2022-09-14 17:26:10 +0200

    Fix test-setup

--- test-setup.ts
+++ test-setup.ts
@@ -1,29 +1,6 @@
 // @ts-ignore
 globalThis.isNodeTestEnv = true
 
-class File extends Blob {
-  name: string
-  constructor(a: any, name: string) {
-    super(a)
-    this.name = name
-  }
-}
-
-// @ts-ignore
-globalThis.File = File
-
-// @ts-ignore
-globalThis.FILE_OBSERVER = false
-
-// // @ts-ignore
-// globalThis.localStorage = new MemStorage()
-
-// // @ts-ignore
-// globalThis.navigator = { language: "en", userAgent: "" }
-
-// // @ts-ignore
-// globalThis.navigator = { language: "en", userAgent: "" }
-
 import { webcrypto } from "crypto"
 
 if (globalThis.crypto == null) {

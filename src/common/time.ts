e66d6f9ffaf40820ad0c01495fe3d2c6b38c804e
Dirk Holtwick <dirk.holtwick@gmail.com>
2023-03-14 11:33:09 +0100

    Duration

--- src/common/time.ts
+++ src/common/time.ts
@@ -35,3 +35,29 @@ export function parseDate(
     }
   }
 }
+
+/**
+ * @returns Timestamp in miliseconds
+ */
+export const getPerformanceTimestamp = (): number => typeof performance !== 'undefined' ? performance.now() : Date.now()
+
+export function duration() {
+  const t0 = getPerformanceTimestamp()
+
+  return function () {
+    const duration = getPerformanceTimestamp() - t0
+    return formatMilliseconds(duration)
+
+    // if (duration > 500)
+    //   return `${(duration / 1000).toFixed(4)}s`
+
+    // // https://elijahmanor.com/format-js-numbers
+    // // https://tc39.es/proposal-unified-intl-numberformat/section6/locales-currencies-tz_proposed_out.html#sec-issanctionedsimpleunitidentifier
+    // return duration.toLocaleString('en-US', {
+    //   style: 'unit',
+    //   unit: 'millisecond',
+    //   notation: 'compact',
+    //   compactDisplay: 'long',
+    // })
+  }
+}

98d131d45e28bc6649300d798b04f311dfa6af3d
Dirk Holtwick <dirk.holtwick@gmail.com>
2023-03-31 11:06:02 +0200

    Better toValidFilename

--- src/common/data/path.ts
+++ src/common/data/path.ts
@@ -1,15 +1,17 @@
 // https://github.com/sindresorhus/filenamify/blob/main/filenamify.js
-
 /* eslint-disable no-control-regex */
+
+import { escapeRegExp } from './regexp'
 
 const MAX_FILENAME_LENGTH = 100
 
-const reControlChars = /[\u0000-\u001F\u0080-\u009F]/g
+// const reControlChars = /[\u0000-\u001F\u0080-\u009F]/g
 const reRelativePath = /^\.+/
 const reTrailingPeriods = /\.+$/
+const reFilenameIllegal = /[<>:\"/\\|?*\u0000-\u001F\u0080-\u009F]+/gm // https://superuser.com/questions/358855/what-characters-are-safe-in-cross-platform-file-names-for-linux-windows-and-os/358861#358861
 
 function filenameReservedRegex() {
-  return /[<>:"/\\|?*\u0000-\u001F]/g
+  return reFilenameIllegal // /[<>:"/\\|?*\u0000-\u001F]/g
 }
 
 function windowsReservedNameRegex() {
@@ -17,31 +19,23 @@ function windowsReservedNameRegex() {
 }
 
 /** Ensure a valid file name. Unsupported characters get a `replacement`, default: `_` */
-export function toValidFilename(
-  string: string,
-  replacement = '_',
-): string | never {
+export function toValidFilename(string: string, replacement = '_'): string | never {
   if (typeof string !== 'string')
     throw new TypeError('Expected a string')
 
-  if (
-    filenameReservedRegex().test(replacement)
-    && reControlChars.test(replacement)
-  ) {
-    throw new Error(
-      'Replacement string cannot contain reserved filename characters',
-    )
-  }
+  if (filenameReservedRegex().test(replacement))
+    throw new Error('Replacement string cannot contain reserved filename characters')
+
+  const reReplacement = `(${escapeRegExp(replacement)})+`
 
   string = string
     .replace(filenameReservedRegex(), replacement)
-    .replace(reControlChars, replacement)
     .replace(reRelativePath, replacement)
     .replace(reTrailingPeriods, '')
+    .replace(new RegExp(reReplacement, 'gm'), replacement) // collapse
+    .trim()
 
-  string = windowsReservedNameRegex().test(string)
-    ? string + replacement
-    : string
+  string = windowsReservedNameRegex().test(string) ? string + replacement : string
 
   return string.slice(0, MAX_FILENAME_LENGTH)
 }

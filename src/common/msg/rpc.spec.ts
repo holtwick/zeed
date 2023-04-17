4292779b2ef3b5b599173d40fc75ef2fcb62e50d
Dirk Holtwick <dirk.holtwick@gmail.com>
2023-04-17 23:33:47 +0200

    useRPCHub

--- src/common/msg/rpc.spec.ts
+++ src/common/msg/rpc.spec.ts
@@ -1,5 +1,6 @@
 import { MessageChannel } from 'worker_threads'
-import { useRPC } from './rpc'
+import { useRPC, useRPCHub } from './rpc'
+import { decodeJson, encodeJson } from '../bin'
 
 let bobCount = 0
 
@@ -25,11 +26,18 @@ type BobFunctions = typeof Bob
 type AliceFunctions = typeof Alice
 
 describe('rpc', () => {
+  beforeEach(() => {
+    bobCount = 0
+  })
+
   it('basic', async () => {
     const channel = new MessageChannel()
 
-    const serialize = (data: any) => JSON.stringify(data)
-    const deserialize = (data: any) => JSON.parse(data)
+    // const serialize = (data: any) => JSON.stringify(data)
+    // const deserialize = (data: any) => JSON.parse(data)
+
+    const serialize = (data: any) => encodeJson(data)
+    const deserialize = (data: any) => decodeJson(data)
 
     const bob = useRPC<AliceFunctions>(Bob, {
       post: data => channel.port1.postMessage(data),
@@ -37,6 +45,44 @@ describe('rpc', () => {
       serialize,
       deserialize,
     })
+
+    const alice = useRPC<BobFunctions>(Alice, {
+      // mark bob's `bump` as an event without response
+      eventNames: ['bump'],
+      post: data => channel.port2.postMessage(data),
+      on: data => channel.port2.on('message', data),
+      serialize,
+      deserialize,
+    })
+
+    // RPCs
+    expect(await bob.hello('Bob')).toEqual('Hello Bob, my name is Alice')
+    expect(await alice.hi('Alice')).toEqual('Hi Alice, I am Bob')
+
+    // one-way event
+    expect(alice.bump()).toBeUndefined()
+
+    expect(Bob.getCount()).toBe(0)
+    await new Promise(resolve => setTimeout(resolve, 100))
+    expect(Bob.getCount()).toBe(1)
+
+    channel.port1.close()
+    channel.port2.close()
+  })
+
+  it('hub', async () => {
+    const channel = new MessageChannel()
+    const serialize = (data: any) => encodeJson(data)
+    const deserialize = (data: any) => decodeJson(data)
+
+    const bobHub = useRPCHub({
+      post: data => channel.port1.postMessage(data),
+      on: data => channel.port1.on('message', data),
+      serialize,
+      deserialize,
+    })
+
+    let bob = bobHub<AliceFunctions>(Bob)
 
     const alice = useRPC<BobFunctions>(Alice, {
       // mark bob's `bump` as an event without response

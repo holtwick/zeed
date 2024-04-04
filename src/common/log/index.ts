ee7331bef0778d8112c30a67da48a018504fe2bd
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-04-04 17:03:15 +0200

    feat: LogConfig

--- src/common/log/index.ts
+++ src/common/log/index.ts
@@ -1,6 +1,7 @@
 export * from './log'
 export * from './log-base'
 export * from './log-colors'
+export * from './log-config'
 export * from './log-console'
 export * from './log-console-original'
 export * from './log-console-capture'

767bb95405417943cd483f0c53ca20fb952d7300
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-04-04 23:51:52 +0200

    fix: LogMemeory more robust

--- src/common/log/log-memory.ts
+++ src/common/log/log-memory.ts
@@ -1,3 +1,4 @@
+import { objectPlain } from '../data'
 import { getTimestamp } from '../time'
 import type { LogHandler, LogHandlerOptions, LogMessage, LogMessageCompact } from './log-base'
 import { useLevelFilter, useNamespaceFilter } from './log-filter'
@@ -12,20 +13,34 @@ export function LoggerMemoryHandler(
   opt: LogHandlerOptions & {
     compact?: boolean
     messages: LogMessageCompact[] | LogMessage[]
+    errorTrace?: boolean
+    limit?: number
+    maxDepth?: number
   },
 ): LogHandler {
-  const { level = undefined, filter = undefined, compact = false, messages = [] } = opt
+  const { level = undefined, filter = undefined, compact = false, messages = [], errorTrace = false, maxDepth = 20 } = opt
   const matchesNamespace = useNamespaceFilter(filter)
   const matchesLevel = useLevelFilter(level)
+
+  let counter = opt.limit ?? Number.POSITIVE_INFINITY
+
   return (msg: LogMessage) => {
     if (!matchesLevel(msg.level))
       return
     if (!matchesNamespace(msg.name))
       return
+    
     msg.timestamp ??= getTimestamp()
+
+    const m = objectPlain(msg, { maxDepth, errorTrace })
     if (compact === true)
-      (messages as LogMessageCompact[]).push([msg.timestamp, msg.level, msg.name, ...msg.messages])
+      (messages as LogMessageCompact[]).push([m.timestamp, m.level, m.name, ...m.messages])
     else
-      (messages as LogMessage[]).push(msg)
+      (messages as LogMessage[]).push(m)
+
+    if (counter <= 0)
+      messages.shift()
+    else
+      --counter
   }
 }

8118a2e41f6c7bbe952b0f45ffd8cc3ac5fa88a9
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-04-09 13:36:03 +0200

    fix: rpc without exceptions

--- src/common/msg/rpc.ts
+++ src/common/msg/rpc.ts
@@ -18,6 +18,8 @@ export interface RPCOptionsBasic extends Pipe {
   onError?: (error: Error, functionName: string, args: any[]) => boolean | void
   /** Custom error handler for timeouts */
   onTimeoutError?: (functionName: string, args: any[]) => boolean | void
+  /** Throw execptions. Default: true */
+  exceptions?: boolean
 }
 
 export interface RPCOptions<Remote> extends RPCOptionsBasic {
@@ -68,6 +70,7 @@ function setupRPCBasic(options: RPCOptionsBasic, functions: any, eventNames: str
     onError,
     onTimeoutError,
     onlyEvents = false,
+    exceptions = true,
   } = options
 
   const rpcPromiseMap = new Map<number, {
@@ -111,7 +114,7 @@ function setupRPCBasic(options: RPCOptionsBasic, functions: any, eventNames: str
         const promise = rpcPromiseMap.get(id)
         if (promise != null) {
           clearTimeout(promise.timeoutId)
-          if (mode === RPCMode.reject)
+          if (mode === RPCMode.reject && exceptions === true)
             promise.reject(method)
           else
             promise.resolve(method)
@@ -146,7 +149,10 @@ function setupRPCBasic(options: RPCOptionsBasic, functions: any, eventNames: str
               throw new Error(`rpc timeout on calling "${method}"`)
             }
             catch (e) {
-              reject(e)
+              if (exceptions === true)
+                reject(e)
+              else
+                resolve(undefined)
             }
             rpcPromiseMap.delete(id)
           }, timeout).unref?.()

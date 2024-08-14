d3f24cc67f64360eba4ca73b5e2ea70fd14106b1
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-08-14 17:37:02 +0200

    feat: eslintDefaults

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,4 @@
 import antfu from '@antfu/eslint-config'
+import { eslintDefaults } from './src/common/eslint-defaults.js'
 
-export default antfu(
-  {
-    vue: true,
-    typescript: true,
-    // typescript: {
-    //   tsconfigPath: './tsconfig.json',
-    // },
-    ignores: [
-      'dist',
-      'node_modules',
-      'build',
-      'tmp',
-      // 'demos',
-      'docs',
-      'coverage',
-      '_archive',
-      // '*.md',
-      // '*.spec.*',
-      // 'vitest.config.ts',
-    ],
-    rules: {
-      'unused-imports/no-unused-vars': 'off',
-      'ts/no-unsafe-assignment': 'off',
-      'ts/no-unsafe-return': 'off',
-      'ts/no-unsafe-member-access': 'off',
-      'ts/no-unsafe-argument': 'off',
-      'ts/no-unsafe-call': 'off',
-      'ts/restrict-template-expressions': 'off',
-      'ts/no-misused-promises': 'off',
-      // 'ts/no-floating-promises': 'error',
-      // 'ts/require-await': 'error',
-
-      // All import properties in one line
-      'object-curly-newline': ['error', {
-        ImportDeclaration: 'never',
-        // ExportDeclaration: 'always'
-      }],
-
-    },
-  },
-)
+export default antfu(eslintDefaults())

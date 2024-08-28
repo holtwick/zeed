ffa15540ca956ee4f4725adc8df1216146f1ae8f
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-08-28 08:06:38 +0200

    feat: migrate schema to class

--- src/common/schema/schema.ts
+++ src/common/schema/schema.ts
@@ -3,106 +3,80 @@ import { first, isBoolean, isFunction, isInteger, isNumber, isObject, isString }
 export interface TypeProps {
 }
 
-export type TypeNames = string
-
 // export interface TypeAssert {
 //   fn: (obj: any) => boolean | never
 //   message?: string
 // }
 
-export interface Type<T = unknown> {
-  type: TypeNames
+interface Type<T = unknown> {
+  readonly type: string
+  readonly _check: (obj: any) => boolean
+  optional: () => Type<T | undefined>
+  default: (value: any) => Type<T | undefined>
+  parse: (obj: any) => T
+  map: (obj: any, fn: (this: Type<T>, obj: any, schema: Type<T>) => any) => any
+  props: (props: TypeProps) => Type<T>
+}
 
-  // _value?: T
-  _object?: Record<string, Type>
-  _union?: Type[]
+class TypeClass<T = unknown> implements Type<T> {
+  readonly type
+  readonly _check
 
-  _check: (obj: any) => boolean
-
-  // _asserts?: TypeAssert[]
-  // assert: (fn: (obj:any)=> boolean, message?:string) => Type<T>
+  constructor(name: string, check?: (obj: any) => boolean) {
+    this.type = name
+    this._check = check ?? (() => true)
+  }
 
   _optional?: boolean
-  optional: () => Type<T | undefined>
 
-  // !!! this causes errors !!! No idea why yet!
-  // default: (value: T | (() => T)) => Type<T | undefined>
+  optional() {
+    this._optional = true
+    return this
+  }
 
-  _default?: T | (() => T)
-  default: (value: any) => Type<T | undefined>
+  _default?: T
 
-  parse: (obj: any, opt?: { // todo obj: T ?
-    transform?: boolean
-    strict?: boolean
-  }) => T
+  default(value: any) {
+    this._default = value
+    return this
+  }
+
+  parse(obj: any): T {
+    if (obj == null) {
+      if (this._default != null) {
+        if (isFunction(this._default))
+          obj = this._default()
+        else
+          obj = this._default
+      }
+    }
+    if (obj == null && this._optional === true)
+      return undefined as any
+    if (obj == null)
+      throw new Error('cannot be undefined')
+    if (!this._check || this._check(obj))
+      return obj
+    throw new Error('wrong value')
+  }
+
+  map(obj: any, fn: (this: Type<T>, obj: any, schema: Type<T>) => any): any {
+    return fn.call(this, obj, this) ?? obj
+  }
 
   _props?: TypeProps
-  props: (props: TypeProps) => Type<T>
 
-  map: (obj: any, fn: (this: Type<T>, obj: any, schema: Type<T>) => any) => any
+  props(props: TypeProps) {
+    this._props = props
+    return this
+  }
 }
 
 export type Infer<T> = T extends Type<infer TT> ? TT : never
 
-type ObjectFixOptional<T> = {
-  [K in keyof T as undefined extends T[K] ? K : never]?: T[K] & {}
-} & {
-  [K in keyof T as undefined extends T[K] ? never : K]: T[K] & {}
-}
-
-type ObjectPretty<V> = Extract<{ [K in keyof V]: V[K] }, unknown>
-
-export type TypeObject<T = unknown> = Type<ObjectPretty<ObjectFixOptional<{
-  [K in keyof T]: Infer<T[K]>
-}>>>
-
 // Helper
 
-function preParse<T>(obj: T, info: Type<T>): T {
-  if (obj == null) {
-    if (info._default != null) {
-      if (isFunction(info._default))
-        obj = info._default()
-      else
-        obj = info._default
-    }
-  }
-  if (obj == null && info._optional === true)
-    return undefined as any
-  if (obj == null)
-    throw new Error('cannot be undefined')
-  if (!info._check || info._check(obj))
-    return obj
-  throw new Error('wrong value')
-}
-
-function generic<T>(type: TypeNames, opt?: Partial<Type<T>>): Type<T> {
-  const info: Type<T> = {
-    parse(obj) {
-      return preParse(obj, this as any)
-    },
-    map(obj, fn) {
-      return fn.call(this, obj, this) ?? obj
-    },
-    _check() {
-      return true
-    },
-    ...opt,
-    type,
-    optional() {
-      this._optional = true
-      return this as any
-    },
-    default(value) {
-      this._default = value
-      return this as any
-    },
-    props(props) {
-      this._props = props
-      return this as any
-    },
-  }
-  return info
+function generic<T>(type: string, opt?: Partial<Type<T>>): Type<T> {
+  return new TypeClass<T>(type, opt?._check)
 }
 
 // Primitives
@@ -135,41 +109,67 @@ export function boolean() {
 
 // Object
 
-export function object<T>(tobj: T): TypeObject<T> {
-  const info = generic('object', {
-    _object: tobj as any,
-    parse(obj) {
-      if (obj == null && this._optional === true)
-        return undefined
-      const newObj: any = {}
-      if (!isObject(obj))
-        return new Error('expected object input')
-      if (!isObject(this._object))
-        return new Error('expected object definition')
-      for (const [key, info] of Object.entries(this._object)) {
-        const value = info.parse((obj as any)[key])
+type ObjectFixOptional<T> = {
+  [K in keyof T as undefined extends T[K] ? K : never]?: T[K] & {}
+} & {
+  [K in keyof T as undefined extends T[K] ? never : K]: T[K] & {}
+}
+
+type ObjectPretty<V> = Extract<{ [K in keyof V]: V[K] }, unknown>
+
+export type TypeObject<T = unknown> = Type<ObjectPretty<ObjectFixOptional<{
+  [K in keyof T]: Infer<T[K]>
+}>>>
+
+class TypeObjectClass<T, O = TypeObject<T>> extends TypeClass<O> {
+  _object: T
+
+  constructor(obj: T) {
+    super('object', () => true)
+    this._object = obj
+  }
+
+  parse(obj: any) {
+    if (obj == null && this._optional === true)
+      return undefined
+    const newObj: any = {}
+    if (!isObject(obj))
+      return new Error('expected object input')
+    if (!isObject(this._object))
+      return new Error('expected object definition')
+    for (const [key, info] of Object.entries(this._object)) {
+      const value = info.parse((obj as any)[key])
+      if (value !== undefined)
+        newObj[key] = value
+    }
+    return newObj
+  }
+
+  map(obj: any, fn: (this: Type<O>, obj: any, schema: Type<O>) => any): any {
+    const result = fn.call(this as any, obj, this as any)
+    if (result !== undefined)
+      return result
+    const newObj: any = {}
+    if (obj) {
+      for (const [key, info] of Object.entries(this._object ?? {})) {
+        const value = (info as Type).map((obj as any)[key], fn as any)
         if (value !== undefined)
           newObj[key] = value
       }
-      return newObj
-    },
-    map(obj, fn) {
-      const result = fn.call(this as any, obj, this as any)
-      if (result !== undefined)
-        return result
-      const newObj: any = {}
-      if (obj) {
-        for (const [key, info] of Object.entries(this._object ?? {})) {
-          const value = info.map((obj as any)[key], fn)
-          if (value !== undefined)
-            newObj[key] = value
-        }
-      }
-      return newObj
-    },
-  })
-  return info
+    }
+    return newObj
+  }
 }
+
+export function object<T>(tobj: T): TypeObject<T> {
+  return new TypeObjectClass(tobj) as any
+}
+
+const x = object({
+  name: string(),
+})
+
+type xt = Infer<typeof x>
 
 // Union
 
@@ -177,10 +177,10 @@ type TransformToUnion<T extends (Type<any>)[]> = T extends Array<infer U> ? Infe
 
 export function union<T extends (Type<any>)[]>(options: T): Type<TransformToUnion<T>> {
   return generic<any>(first(options)?.type ?? 'any', {
-    _union: options,
-    _check(obj) {
-      return this._union?.some(t => t._check(obj)) ?? true
-    },
+    // _union: options,
+    // _check(obj) {
+    //   return this._union?.some(t => t._check(obj)) ?? true
+    // },
   })
 }
 
@@ -203,7 +203,7 @@ export function stringLiterals<const T extends readonly string[], O = T[number]>
 // Function
 
 type TupleOutput<T extends Type[]> = {
-  [K in keyof T]: T[K] extends Type<infer U> ? U : never;
+  [K in keyof T]: T[K] extends Type<infer U> ? U : never
 }
 
 type ArrayOutput<Head extends Type[], Rest extends Type | undefined> = [

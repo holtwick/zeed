ff4dda8da72100e48a7a4a11d4c86f65e4bac7da
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-10-26 15:20:41 +0200

    fix: docs for decimal and faster rounding alg used

--- src/common/data/decimal.ts
+++ src/common/data/decimal.ts
@@ -1,6 +1,10 @@
 export type DecimalValue = number
 export type DecimalInput = number | string | DecimalValue
 
+/** 
+ * The float returned is guaraneteed to have only the `decimalPlaces` length,
+ * no rounding errors or float arithmetic artefacts. 
+ */
 export function decimal(
   value: DecimalInput,
   decimalPlaces = 2,
@@ -8,6 +12,7 @@ export function decimal(
   return +(+value).toFixed(decimalPlaces)
 }
 
+/** `123` becomes `1.23` */
 export function decimalFromCents(
   value: DecimalInput,
   decimalPlaces = 2,
@@ -15,6 +20,7 @@ export function decimalFromCents(
   return +(+value / 10 ** decimalPlaces).toFixed(decimalPlaces)
 }
 
+/** `1.23` becomes `123` */
 export function decimalToCents(
   value: DecimalInput,
   decimalPlaces = 2,
@@ -22,6 +28,7 @@ export function decimalToCents(
   return Math.round(+value * 10 ** decimalPlaces)
 }
 
+/** `1.23` becomes `23` */
 export function decimalCentsPart(
   value: DecimalInput,
   decimalPlaces = 2,

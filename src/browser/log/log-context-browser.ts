9a80ab95838a504b63c35c41e3c97c02475e9e88
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-11-07 16:13:08 +0100

    fix: better localStorage fix

--- src/browser/log/log-context-browser.ts
+++ src/browser/log/log-context-browser.ts
@@ -3,14 +3,13 @@ import type { LogConfig } from '../../common/log/log-config'
 import { isEmpty } from '../../common/data/is'
 import { getGlobalLogger } from '../../common/log/log'
 import { _LoggerFromConfig } from '../../common/log/log-config'
-import { getLocalStorage, isBrowser } from '../../common/platform'
+import { isBrowser } from '../../common/platform'
 import { LoggerBrowserHandler } from './log-browser'
 import { LoggerBrowserSetupDebugFactory } from './log-browser-factory'
 
 export function Logger(name?: string, level?: LogLevelAliasType): LoggerInterface {
   return getGlobalLogger((context) => {
-    const localStorage = getLocalStorage()
-    if (isBrowser() && !isEmpty(localStorage?.getItem('zeed'))) {
+    if (isBrowser() && (typeof localStorage !== 'undefined' ? !isEmpty(localStorage?.getItem('zeed')) : false)) {
       context.setHandlers([LoggerBrowserHandler()]) // Fallback for previously registered Loggers
       context.setFactory(LoggerBrowserSetupDebugFactory({}))
     }
